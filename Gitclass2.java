e331d4abd78637d97ba0344cb22ff638f6e4e65b
testautosel <46587141+testautosel@users.noreply.github.com>
2019-01-17 20:53:42 +0530

    Add files via upload

---
+++ Gitclass2.java
@@ -0,0 +1,10 @@
+package gitupload;
+
+public class Gitclass2 {
+
+	public void test() {
+	
+	System.out.println("Gittest2");
+	
+}
+}
---
+++ gitclass.java
@@ -0,0 +1,10 @@
+package gitupload;
+
+public class gitclass {
+
+	public void test() {
+	
+	System.out.println("Gittest");
+	
+}
+}
